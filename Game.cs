e6f339776db97948ce3ddfb743b99677d77caafe
esraa <esraaemary33@gmail.com>
2025-07-23 13:59:19 +0200

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_Game
+{
+    internal class Game
+    {
+        Dictionary<string, int> scores = new Dictionary<string, int>();
+
+
+    }
+}
